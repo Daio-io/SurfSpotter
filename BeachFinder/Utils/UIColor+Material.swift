9e1eac255a34e603b0dbd85e53547dc8f3a43d12
Daio-io <dai.williams@bbc.co.uk>
2016-02-29 11:41:38 +0000

    Add grey to material colours

--- BeachFinder/Utils/UIColor+Material.swift
+++ BeachFinder/Utils/UIColor+Material.swift
@@ -10,12 +10,15 @@ import UIKit
 
 extension UIColor {
     
-    static var blue600: UIColor {
+    static var Blue600: UIColor {
         get { return UIColor(colorLiteralRed: 0.118, green: 0.533, blue: 0.898, alpha: 1) }
     }
     
-    static var amber500: UIColor {
+    static var Amber500: UIColor {
         get { return UIColor(colorLiteralRed: 1, green: 0.757, blue: 0.027, alpha: 1) }
     }
     
+    static var BlueGrey50: UIColor {
+        get { return UIColor(red:0.93, green:0.94, blue:0.95, alpha:1) }
+    }
 }
