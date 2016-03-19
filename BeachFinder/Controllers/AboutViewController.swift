f9c1b8d693d6edca6c0d8ae2ef07323a1c633d34
Daio-io <dai.williams@bbc.co.uk>
2016-03-19 20:11:10 +0000

    Added license.html for about page

---
+++ BeachFinder/Controllers/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BeachFinder
+//
+//  Created by Dai on 19/03/2016.
+//  Copyright © 2016 daio. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
