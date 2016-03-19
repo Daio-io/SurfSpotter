9b59dd6a74ea557c1d01cfde7e9bffa757c3bd5c
Daio-io <dai.williams@bbc.co.uk>
2016-03-19 11:15:35 +0000

    Added temp api-keys i can check in while testing - Added early demo of app

--- BeachFinder/AppDelegate.swift
+++ BeachFinder/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        // Temp key I can check in
         GMSServices.provideAPIKey("AIzaSyAIZSWDpqd8sEaGfwBBarzh6QRg4XvuQ-k")
         
         displayMainViewController()
