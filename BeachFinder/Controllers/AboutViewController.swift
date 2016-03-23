307893c1991424a4ea52d7ec91bf93d0f15b8826
Daio-io <dai.williams@bbc.co.uk>
2016-03-23 00:15:16 +0000

    - Renamed to license view controller and made cleaner about view controller

--- BeachFinder/Controllers/AboutViewController.swift
+++ BeachFinder/Controllers/AboutViewController.swift
@@ -1,57 +1,29 @@
 //
-//  AboutViewController.swift
+//  SettingsViewController.swift
 //  BeachFinder
 //
-//  Created by Dai on 19/03/2016.
+//  Created by Dai on 22/03/2016.
 //  Copyright © 2016 daio. All rights reserved.
 //
 
 import UIKit
 
-class AboutViewController: UIViewController, UIWebViewDelegate {
-    
-    @IBOutlet private weak var webView: UIWebView!
+class AboutViewController: UIViewController {
     
     init() {
-        super.init(nibName:"AboutViewController", bundle:nil)
+        super.init(nibName: "AboutViewController", bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("Not implemented")
+        fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        webView.delegate = self
-        webView.scalesPageToFit = true
-        loadLicense()
+    override func viewDidAppear(animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
-    override func viewWillAppear(animated: Bool) {
-        navigationController?.setNavigationBarHidden(false, animated: false)
+    @IBAction func licenseClicked(sender: AnyObject) {
+        navigationController?.presentViewController(LicenseViewController(), animated: true, completion: nil)
     }
-    
-    func loadLicense() {
-        if let path = NSBundle.mainBundle().pathForResource("license", ofType: "html") {
-            let url = NSURL(string: path)
-            let request = NSURLRequest(URL: url!)
-            webView.loadRequest(request)
-        }
-    }
-    
-    @IBAction func dismissClicked(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    // MARK - UIWebViewDelegate
-    
-    // Ensure links in the webview are opened in Safari
-    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        if navigationType == UIWebViewNavigationType.LinkClicked {
-            UIApplication.sharedApplication().openURL(request.URL!)
-            return false
-        }
-        return true
-    }
-    
+
 }
---
+++ BeachFinder/Controllers/LicenseViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AboutViewController.swift
+//  BeachFinder
+//
+//  Created by Dai on 19/03/2016.
+//  Copyright © 2016 daio. All rights reserved.
+//
+
+import UIKit
+
+class LicenseViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet private weak var webView: UIWebView!
+    
+    init() {
+        super.init(nibName:"LicenseViewController", bundle:nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Not implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.delegate = self
+        webView.scalesPageToFit = true
+        loadLicense()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    func loadLicense() {
+        if let path = NSBundle.mainBundle().pathForResource("license", ofType: "html") {
+            let url = NSURL(string: path)
+            let request = NSURLRequest(URL: url!)
+            webView.loadRequest(request)
+        }
+    }
+    
+    @IBAction func dismissClicked(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK - UIWebViewDelegate
+    
+    // Ensure links in the webview are opened in Safari
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if navigationType == UIWebViewNavigationType.LinkClicked {
+            UIApplication.sharedApplication().openURL(request.URL!)
+            return false
+        }
+        return true
+    }
+    
+}
